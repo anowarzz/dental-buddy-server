006806fe54df93124a93c9de5d9c85f6d2394cad
anowarzz <anowarhosen444@gmail.com>
2022-11-09 10:57:30 +0600

    server deployed to vercel

--- index.js
+++ index.js
@@ -11,19 +11,45 @@ app.use(cors());
 app.use(express.json());
 
 
-// MongoDb Password : 
-// user : 
-// 
+
 
 // Mongo DB
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v1rp4a3.mongodb.net/?retryWrites=true&w=majority`;
-
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+// Function for CRUD Operation
+
+async function run () {
+    try {
+        const serviceCollection = client.db('dentalBuddy').collection('services')
+
+
+        //Services API - Loading All Services from DB
+
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+
+
+    }
+
+
+
+
+
+
+
+    finally{
+
+    }
+}
+
+run().catch(error => console.error(error))
 
 
 
