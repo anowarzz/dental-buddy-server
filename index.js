6da73f626e6883fad4a08e8adc8e56d4d2e2801f
anowarzz <anowarhosen444@gmail.com>
2022-11-09 10:15:20 +0600

    added mongodb server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +9,20 @@ const port = process.env.PORT || 5000;
 // Middle Wares
 app.use(cors());
 app.use(express.json());
+
+
+// MongoDb Password : 
+// user : 
+// 
+
+// Mongo DB
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v1rp4a3.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 
 
