6e242a1a1ecc2ae050316297fa0c67e1efd1dc57
anowarzz <anowarhosen444@gmail.com>
2022-11-09 23:21:29 +0600

    added single service api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -35,11 +35,24 @@ async function run () {
         })
 
         //Services API - Loading All services  from DB
-        app.get('/all-services', async(req, res) => {
+        app.get('/services', async(req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query)
             const services = await cursor.toArray();
             res.send(services)
+        })
+
+
+        // Loading Single Service Details from DB
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+            console.log(service);
+            
         })
 
 
